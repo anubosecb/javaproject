cdb7b9a6cdf27ecba9a6338e13c18e6ea9718e07
anubosecb <135810718+anubosecb@users.noreply.github.com>
2023-08-10 08:49:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ import java.io.*;
 class HelloWorld {
 	      public static void main(String args[]) {
 		                System.out.println("Hello World from Java");
-		      System.out.println("code changed");
+		      System.out.println("code changed for trigger ");
 		      
 				      }
 }
